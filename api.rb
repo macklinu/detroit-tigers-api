b2106115a5bb4adce579a1d3c9b97c54797a72c2
Macklin Underdown <macklinu@gmail.com>
2017-04-09 21:13:21 -0400

    Namespace API and remove unnecessary endpoints
    - Remove '/'
    - Remove '/games/all'
    - Move '/games/upcoming' to '/v1/games/upcoming'

--- api.rb
+++ api.rb
@@ -3,6 +3,7 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require 'sinatra/json'
+require 'sinatra/namespace'
 require 'sinatra/reloader' if development?
 require './game.rb'
 
@@ -10,16 +11,10 @@ before do
   content_type :json
 end
 
-get '/' do
-  'Hello World!'
-end
-
-get '/games/all' do
-  json Game.all
-end
-
-get '/games/upcoming' do
-  json Game.where(date: Date.today..Date.today + 6.days)
+namespace '/v1' do
+  get '/games/upcoming' do
+    json Game.where(date: Date.today..Date.today + 6.days)
+  end
 end
 
 not_found do
--- spec/api_spec.rb
+++ spec/api_spec.rb
@@ -1,56 +1,35 @@
 # frozen_string_literal: true
 
 describe 'Detroit Tigers API' do
-  describe 'GET /' do
-    it 'should return Hello World!' do
-      get '/'
+  describe '/v1' do
+    describe 'GET /games/upcoming' do
+      it 'returns the next 7 days of games by default' do
+        now = Date.today
+        six_days_from_now = now + 6.days
+        (now..six_days_from_now).each do |date|
+          create(:game, date: date)
+        end
 
-      expect_status(200)
-      expect(body).to eql 'Hello World!'
-    end
-  end
-  describe 'GET /games/all' do
-    it 'returns all games in the database' do
-      num_games = 3
-      create_list(:game, num_games)
+        get '/v1/games/upcoming'
 
-      get '/games/all'
-
-      expect_status(200)
-      expect_json_sizes(num_games)
-      expect_json_types('*', id: :int,
-                             opponent: :string,
-                             date: :date,
-                             time: :date)
-    end
-  end
-  describe 'GET /games/upcoming' do
-    it 'returns the next 7 days of games by default' do
-      now = Date.today
-      six_days_from_now = now + 6.days
-      (now..six_days_from_now).each do |date|
-        create(:game, date: date)
+        expect_status(200)
+        expect_json_sizes(7)
+        expect_json_types('*', id: :int,
+                               opponent: :string,
+                               date: :date,
+                               time: :date)
+        expect_json('0', date: now.strftime('%F'))
+        expect_json('6', date: six_days_from_now.strftime('%F'))
       end
-
-      get '/games/upcoming'
-
-      expect_status(200)
-      expect_json_sizes(7)
-      expect_json_types('*', id: :int,
-                             opponent: :string,
-                             date: :date,
-                             time: :date)
-      expect_json('0', date: now.strftime('%F'))
-      expect_json('6', date: six_days_from_now.strftime('%F'))
     end
-  end
-  describe 'Error handling' do
-    describe '404' do
-      it 'returns proper error message' do
-        get '/invalid_endpoint'
+    describe 'Error handling' do
+      describe '404' do
+        it 'returns proper error message' do
+          get '/invalid_endpoint'
 
-        expect_status(404)
-        expect_json(message: 'Not found')
+          expect_status(404)
+          expect_json(message: 'Not found')
+        end
       end
     end
   end
